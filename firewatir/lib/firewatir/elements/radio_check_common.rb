3e68bd9b9f318fc9b4e24e7703abaf1afa107e88
ethan <ethan@medidata>
2009-11-19 13:31:08 -0500

    make highlight on radios and checkboxes do something in firefox, where most styling is ignored. now they change size by a few pixels to indicate highlight, since I can't find any other way to change radios and checkboxes visually.

--- firewatir/lib/firewatir/elements/radio_check_common.rb
+++ firewatir/lib/firewatir/elements/radio_check_common.rb
@@ -1,6 +1,25 @@
 module Watir
+  module FFRadioCheckboxCommon
+    private
+    # radios in firefox seem to be totally unresponsive to css color and border, but
+    # they can change size, so use that for the highlight method. 
+    # see http://www.456bereastreet.com/lab/styling-form-controls-revisited/radio-button/
+    def set_highlight(options={})
+      assert_exists do
+        @original_height=element_object.offsetHeight
+        @original_width=element_object.offsetWidth
+        element_object.style.height=@original_height+3
+        element_object.style.width=@original_width+3
+      end
+    end
+    def clear_highlight(options={})
+      element_object.style.height=@original_height
+      element_object.style.width=@original_width
+    end
+  end
   class FFRadio < FFInputElement
     include Radio
+    include FFRadioCheckboxCommon
   end # Radio
 
   #
@@ -9,5 +28,6 @@ module Watir
   #
   class FFCheckBox < FFInputElement
     include CheckBox
+    include FFRadioCheckboxCommon
   end # CheckBox
 end # FireWatir

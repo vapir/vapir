0ab4fa3e438a051083a1e611aa0e330cc58783f2
Ethan <ethan@medidata>
2010-03-02 17:59:24 -0500

    move assertions file into testcase; only place it gets included.

--- vapir-common/lib/vapir-common/testcase.rb
+++ vapir-common/lib/vapir-common/testcase.rb
@@ -1,7 +1,38 @@
 require 'test/unit'
-require 'vapir-common/assertions'
+require 'test/unit/assertions'
 
 module Vapir
+  # Verification methods
+  module Assertions
+    include Test::Unit::Assertions
+    
+    # Log a failure if the boolean is true. The message is the failure
+    # message logged.
+    # Whether true or false, the assertion count is incremented.
+    def verify boolean, message = 'verify failed.'
+      add_assertion
+      add_failure message.to_s, caller unless boolean
+    end
+    
+    def verify_equal expected, actual, message=nil
+      full_message = build_message(message, <<EOT, expected, actual)
+<?> expected but was
+<?>.
+EOT
+      verify(expected == actual, full_message)
+    end
+    def verify_match pattern, string, message=nil
+      pattern = case(pattern)
+      when String
+        Regexp.new(Regexp.escape(pattern))
+      else
+        pattern
+      end
+      full_message = build_message(message, "<?> expected to be =~\n<?>.", string, pattern)
+      verify(string =~ pattern, full_message)
+    end
+    
+  end
 
   class TestCase < Test::Unit::TestCase
     include Vapir::Assertions

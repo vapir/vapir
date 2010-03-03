ed6021d2da6d41b5c28936ddf2e2b91ab8174568
Ethan <ethan@medidata>
2010-03-03 13:31:42 -0500

    move win32ole extension #respond_to? into win32ole.rb.

--- vapir-ie/lib/vapir-ie/win32ole.rb
+++ vapir-ie/lib/vapir-ie/win32ole.rb
@@ -12,3 +12,34 @@ end
 require 'win32ole'
 
 WIN32OLE.codepage = WIN32OLE::CP_UTF8
+
+# necessary extension of win32ole 
+class WIN32OLE
+  def respond_to?(method)
+    super || object_respond_to?(method)
+  end
+  
+  # checks if WIN32OLE#ole_method returns an WIN32OLE_METHOD, or errors. 
+  # WARNING: #ole_method is pretty slow, and consequently so is this. you are likely to be better
+  # off just calling a method you are not sure exists, and rescuing the WIN32OLERuntimeError
+  # that is raised if it doesn't exist. 
+  def object_respond_to?(method)
+    method=method.to_s
+    # strip assignment = from methods. going to assume that if it has a getter method, it will take assignment, too. this may not be correct, but will have to do. 
+    if method =~ /=\z/
+      method=$`
+    end
+    respond_to_cache[method]
+  end
+  
+  private
+  def respond_to_cache
+    @respond_to_cache||=Hash.new do |hash, key|
+      hash[key]=begin
+        !!self.ole_method(key)
+      rescue WIN32OLERuntimeError
+        false
+      end
+    end
+  end
+end

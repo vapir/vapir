fe0721110713ae5efea0a24ee5b63df7210ebda6
Ethan <ethan@medidata>
2010-05-12 15:50:07 -0400

    clobbering #wait on frames as it should wait on the browser, not just on the frame itself.

--- vapir-ie/lib/vapir-ie/frame.rb
+++ vapir-ie/lib/vapir-ie/frame.rb
@@ -7,6 +7,19 @@ module Vapir
     include Frame
     include IE::PageContainer
     
+    # waiting on a Frame should carry on upwards to the browser - the sorts of operations that we wait after 
+    # (clicking a link or whatever) tend to affect other frames too; waiting on just this frame doesn't 
+    # make sense. 
+    def wait(options={}) # :nodoc:
+      if browser # prefer to wait on the browser
+        browser.wait(options)
+      elsif container # if we don't have the browser, wait on the container (presumably this exists) 
+        container.wait(options)
+      else # but if we don't have a container either, just call to PageContainer#wait (by this alias) 
+        page_container_wait(options)
+      end
+    end
+    
     def content_window_object
       element_object.contentWindow
     end

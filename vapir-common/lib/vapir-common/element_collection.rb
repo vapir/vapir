421cebf92c1a72752d40a89d1c23918e73846615
Ethan <ethan@medidata>
2010-05-27 18:00:51 -0400

    add documentation to element collection methods

--- vapir-common/lib/vapir-common/element_collection.rb
+++ vapir-common/lib/vapir-common/element_collection.rb
@@ -10,26 +10,34 @@ module Vapir
       @extra=extra.merge(:container => container)
     end
 
-    def each
+    # yields each element in the collection to the given block 
+    def each # :yields: element
       candidates.each do |candidate|
         yield @collection_class.new(:element_object, candidate, @extra)
       end
       self
     end
-    def each_index
+    # yields each index from 1..length. 
+    # 
+    # note that if you are using this and accessing each subscript, it will be exponentially slower
+    # than using #each or #each_with_index. 
+    def each_index # :yields: index
       (1..length).each do |i|
         yield i
       end
     end
+    # returns the number of elements in the collection 
     def length
       candidates.length
     end
     alias size length
+    # returns true if this collection contains no elements 
     def empty?
       size==0
     end
     alias each_with_enumerable_index each_with_index # call ruby's 0-based indexing enumerable_index; call ours element_index
-    def each_with_element_index
+    # yields each element and index in the collection 
+    def each_with_element_index # :yields: element, index
       index=1
       candidates.each do |candidate|
         yield @collection_class.new(:index, nil, @extra.merge(:index => index, :element_object => candidate)), index
@@ -50,17 +58,20 @@ module Vapir
     def [](index)
       at(index)
     end
+    # returns the element at the given index in the collection. indices start at 1. 
     def at(index)
       @collection_class.new(:index, nil, @extra.merge(:index => index))
     end
+    # returns the first element in the collection. 
     def first
       at(:first)
     end
+    # returns the last element. this will refer to the last element even if the number of elements changes, assuming relocation. 
     def last
       at(:last)
     end
-    
-    def find(&block)
+    # returns an element for which the given block returns true (that is, not false or nil) when yielded that element 
+    def find(&block) # :yields: element
       element=@collection_class.new(:custom, block, @extra.merge(:locate => false))
       element.exists? ? element : nil
     end
@@ -70,7 +81,7 @@ module Vapir
     end
     alias detect! find!
     
-    def inspect
+    def inspect # :nodoc: 
       "\#<#{self.class.name}:0x#{"%.8x"%(self.hash*2)} #{map{|el|el.inspect}.join(', ')}>"
     end
 
@@ -81,7 +92,7 @@ module Vapir
       matched_candidates(@collection_class.specifiers, @collection_class.all_dom_attr_aliases)
     end
     public
-    def pretty_print(pp)
+    def pretty_print(pp) # :nodoc: 
       pp.object_address_group(self) do
         pp.seplist(self, lambda { pp.text ',' }) do |element|
           pp.breakable ' '

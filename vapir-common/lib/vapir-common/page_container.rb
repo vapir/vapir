7600a8890405f5406318a2f408557a8b20e5b340
Ethan <ethan@medidata>
2010-08-09 01:33:56 -0400

    document_element should be called document_element_object, being a JsshObject or a WIN32OLE

--- vapir-common/lib/vapir-common/page_container.rb
+++ vapir-common/lib/vapir-common/page_container.rb
@@ -4,9 +4,11 @@ module Vapir
     def containing_object
       document_object
     end
-    def document_element
+    def document_element_object
       document_object.documentElement || raise(Exception::ExistenceFailureException, "document_object.documentElement was nil")
     end
+    alias document_element document_element_object
+    
     def title
       document_object.title
     end

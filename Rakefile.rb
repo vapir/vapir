6253499a07e0c57dc445eb90a501f9e40abd8310
Ethan <ethan@medidata>
2010-08-19 23:23:53 -0400

    rake rdoc task to generate the various rdocs for vapir-ie, vapir-firefox, both browsers together, jssh stuff

--- Rakefile.rb
+++ Rakefile.rb
@@ -7,6 +7,54 @@ base = File.dirname(__FILE__)
 end
 require 'vapir-common/external/core_extensions.rb'
 
+desc 'Build rdoc'
+task :rdoc do
+  load 'vapir-ie/vapir-ie.gemspec'
+  load 'vapir-firefox/vapir-firefox.gemspec'
+  load 'vapir-common/vapir-common.gemspec'
+  exclude = %w(
+    vapir-firefox/lib/vapir-firefox/jssh_socket.rb
+    vapir-ie/lib/vapir-ie/win32ole/win32ole.c
+  )
+  rdoc_argvs=[ exclude.map{|file| ['--exclude', file]}.flatten +
+    %W(
+      --title Vapir\ #{Vapir::Common::VERSION}
+      --op vapir_rdoc
+    ) + (Vapir::Common::GemSpec.files.map{|f| File.join('vapir-common', f)} +
+         Vapir::Firefox::GemSpec.files.map{|f| File.join('vapir-firefox', f)} +
+         Vapir::IE::GemSpec.files.map{|f| File.join('vapir-ie', f)} -
+         exclude),
+    %W(
+      --title Vapir-IE\ #{Vapir::IE::VERSION}
+      --op vapir_ie_rdoc
+      --main Vapir::IE
+    ) + (Vapir::Common::GemSpec.files.map{|f| File.join('vapir-common', f)} +
+         Vapir::IE::GemSpec.files.map{|f| File.join('vapir-ie', f)} -
+         exclude),
+    %W(
+      --title Vapir-Firefox\ #{Vapir::Firefox::VERSION}
+      --op vapir_firefox_rdoc
+      --main Vapir::Firefox
+    ) + (Vapir::Common::GemSpec.files.map{|f| File.join('vapir-common', f)} +
+         Vapir::Firefox::GemSpec.files.map{|f| File.join('vapir-firefox', f)} -
+         exclude),
+    %w(
+      --title JsshObject\ JsshSocket
+      --op jssh_rdoc
+      --main JsshObject
+      vapir-firefox/lib/vapir-firefox/jssh_socket.rb
+    ),
+  ].map{|argv| %w(-f html  --tab-width 2  --show-hash  --inline-source  --template hanna  --charset=UTF-8)+argv } # these will be common to all of them
+
+  gem 'hanna'
+  require 'hanna/version'
+  Hanna::require_rdoc
+  require 'rdoc/rdoc'
+  rdoc_argvs.each do |argv|
+    RDoc::RDoc.new.document(argv)
+  end
+end
+
 desc "check files for things that appear wrong: not in a gemfile; contains a carriage return; or wrong mode"
 task :checkfiles do
   should_be_gemfiles = Dir['vapir-*/lib/**/*']-Dir['vapir-ie/lib/vapir-ie/IEDialog/*'].select(&File.method(:file?))

88e2903758131609e179e93f9a5351e9ef136114
Ethan <ethan@unth>
2010-08-20 17:10:06 -0400

    get rid of file of rdoc options, now all set by the rakefile

--- vapir-ie/watir-rdoc.rb
+++
@@ -1,5 +0,0 @@
-$WATIR_RDOC_OPTIONS = [] << 
-      '--title' << 'Vapir API Reference' <<
-      '--accessor' << 'def_wrap=R,def_wrap_guard=R,def_creator=R,def_creator_with_default=R' <<
-      '--main' << 'Vapir::IE'
-$WATIR_EXTRA_RDOC_FILES = ['lib/readme.rb', 'lib/changes.rb', 'lib/license.rb']

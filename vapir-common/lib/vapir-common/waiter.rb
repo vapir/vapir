fc3a4676e4692ec1eab9a120401e7051d97d7061
Ethan <ethan@medidata>
2010-08-20 12:31:18 -0400

    drop watir's version of Waiter. keep the API, but rely in the ::Waiter class instead of TimeKeeper stuff

--- vapir-common/lib/vapir-common/waiter.rb
+++ vapir-common/lib/vapir-common/waiter.rb
@@ -1,94 +1,47 @@
 require 'vapir-common/exceptions'
 
 module Vapir
+  class Waiter # :nodoc:all
+    # How long to wait between each iteration through the wait_until
+    # loop. In seconds.
+    attr_accessor :polling_interval
   
-  def wait_until(*args)
-    Waiter.wait_until(*args) {yield}
-  end
-
-class TimeKeeper
-  attr_reader :sleep_time
-  def initialize 
-    @sleep_time = 0.0
-  end
-  def sleep seconds
-    @sleep_time += Kernel.sleep seconds
-  end
-  def now
-    Time.now
-  end
-end
-
-class Waiter
-  # This is an interface to a TimeKeeper which proxies
-  # calls to "sleep" and "Time.now".
-  # Useful for unit testing Waiter.
-  attr_accessor :timer
-
-  # How long to wait between each iteration through the wait_until
-  # loop. In seconds.
-  attr_accessor :polling_interval
-
-  # Timeout for wait_until.
-  attr_accessor :timeout
-  
-  @@default_polling_interval = 0.5
-  @@default_timeout = 60.0
-
-  def initialize(timeout=@@default_timeout,
-                 polling_interval=@@default_polling_interval)
-    @timeout = timeout
-    @polling_interval = polling_interval
-    @timer = TimeKeeper.new
-  end
-
-  # Execute the provided block until either (1) it returns true, or
-  # (2) the timeout (in seconds) has been reached. If the timeout is reached,
-  # a TimeOutException will be raised. The block will always
-  # execute at least once.
-  # 
-  # waiter = Waiter.new(5)
-  # waiter.wait_until {puts 'hello'}
-  # 
-  # This code will print out "hello" for five seconds, and then raise a 
-  # Vapir::TimeOutException.
-  def wait_until # block
-    start_time = now
-    until yield do
-      if (duration = now - start_time) > @timeout
-        raise Vapir::Exception::TimeOutException.new(duration, @timeout),
-          "Timed out after #{duration} seconds."
-      end
-      sleep @polling_interval
-    end
-  end
-
-  # Execute the provided block until either (1) it returns true, or
-  # (2) the timeout (in seconds) has been reached. If the timeout is reached,
-  # a TimeOutException will be raised. The block will always
-  # execute at least once.
-  # 
-  # Waiter.wait_until(5) {puts 'hello'}
-  # 
-  # This code will print out "hello" for five seconds, and then raise a 
-  # Vapir::TimeOutException.
-
-  # IDEA: wait_until: remove defaults from Waiter.wait_until
-  def self.wait_until(timeout=@@default_timeout,
-                      polling_interval=@@default_polling_interval)
-    waiter = new(timeout, polling_interval)
-    waiter.wait_until { yield }
-  end
-     
-  private
-  def sleep seconds
-    @timer.sleep seconds
-  end
-  def now
-    @timer.now
-  end
-end
+    # Timeout for wait_until.
+    attr_accessor :timeout
     
+    @@default_polling_interval = 0.5
+    @@default_timeout = 60.0
+  
+    def initialize(timeout=@@default_timeout, polling_interval=@@default_polling_interval)
+      @timeout = timeout
+      @polling_interval = polling_interval
+    end
+  
+    module WaitUntil
+      public
+      # Execute the provided block until either (1) it returns true, or
+      # (2) the timeout (in seconds) has been reached. If the timeout is reached,
+      # a TimeOutException will be raised. The block will always
+      # execute at least once.
+      # 
+      # waiter = Waiter.new(5)
+      # waiter.wait_until {puts 'hello'}
+      # 
+      # This code will print out "hello" for five seconds, and then raise a 
+      # Vapir::TimeOutException.
+      def wait_until(timeout=::Vapir::Waiter.send(:class_variable_get, '@@default_timeout'), polling_interval=::Vapir::Waiter.send(:class_variable_get, '@@default_polling_interval'), &block)
+        ::Waiter.try_for(timeout, :interval => polling_interval, &block)
+      end
+    end
+    include ::Vapir::Waiter::WaitUntil
+    extend ::Vapir::Waiter::WaitUntil
+  end
+  include ::Vapir::Waiter::WaitUntil
+  extend ::Vapir::Waiter::WaitUntil
+  class Browser
+    include ::Vapir::Waiter::WaitUntil
+    extend ::Vapir::Waiter::WaitUntil
+  end
 end # module
 
 require 'vapir-common/handle_options'

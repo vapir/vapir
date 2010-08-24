082913853081b2c75959ab199f169c82aa648fdf
Ethan <ethan@medidata>
2010-08-24 12:01:03 -0400

    update version to 1.7.1 for release

--- vapir-common/lib/vapir-common.rb
+++ vapir-common/lib/vapir-common.rb
@@ -1,6 +1,6 @@
 module Vapir
   module Common
-    VERSION = '1.7.1.rc1'
+    VERSION = '1.7.1'
   end
 end
 require 'vapir-common/browser'
--- vapir-firefox/lib/vapir-firefox/version.rb
+++ vapir-firefox/lib/vapir-firefox/version.rb
@@ -1,5 +1,5 @@
 module Vapir
   class Firefox
-    VERSION = '1.7.1.rc1'
+    VERSION = '1.7.1'
   end
 end

903e11d5e422cdd7efc45765bad762163093b957
Ethan <ethan@unth>
2010-08-31 14:01:01 -0400

    update vapir.rb to add the appropriate lib directories to $LOAD_PATH.
    this makes it so that when vapir is used without rubygems, you can just require 'path/to/vapir-common/lib/vapir' and not worry about having vapir-common/lib or the browser-specific libs in your $LOAD_PATH.

--- vapir-common/lib/vapir.rb
+++ vapir-common/lib/vapir.rb
@@ -1,1 +1,9 @@
+base = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+['vapir-common','vapir-ie','vapir-firefox'].each do |lib|
+  libdir = File.join(base, lib, 'lib')
+  if File.directory?(libdir) && !$LOAD_PATH.any?{|lp| File.expand_path(lp) == File.expand_path(libdir) }
+    $LOAD_PATH.unshift(libdir)
+STDERR.puts "-I #{libdir}"
+  end
+end
 require 'vapir-common'

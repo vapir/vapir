65b4d7bb47eec5d84f6bf063e90c23a08f913536
Ethan <ethan@medidata>
2010-10-27 01:02:15 -0400

    add notes about the constants used in IE's ClearTracks module, and define them a little more neatly.

--- vapir-ie/lib/vapir-ie/clear_tracks.rb
+++ vapir-ie/lib/vapir-ie/clear_tracks.rb
@@ -3,14 +3,18 @@ require 'vapir-ie/browser.rb'
 module Vapir
   class IE
     module ClearTracks
-      History = 1
-      Cookies = 2
-      # ?? = 4
-      TemporaryFiles = 8
-      FormData = 16
-      StoredPasswords = 32
-      All = 255
-      FilesAndSettingsStoredByAddOns = 4096
+      History =         1 << 0
+      Cookies =         1 << 1
+      # ?? =             1 << 2
+      TemporaryFiles =  1 << 3
+      FormData =        1 << 4
+      StoredPasswords = 1 << 5
+      # ?? =             1 << 6
+      # ?? =             1 << 7
+      All = (1 << 8)-1 # this is called 'All' by such references as exist, despite higher bits being meaningful too. 
+      FilesAndSettingsStoredByAddOns = 1 << 12 # don't know what happened to any of bits 6 through 11. also this only seems to be documented anywhere as 4351 = 1000011111111 (= All | FilesAndSettingsStoredByAddOns)
+      # apparently 1 << 13 is also meaningful, but I have no idea what it is. I have seen ClearMyTracksByProcess called with the argument 8651 = 10000111001011 (so bits 0, 1, 3, 6, 7, 8, 13)
+      
       def self.clear_tracks(what)
         unless const_defined?('InetCpl')
           require 'ffi'

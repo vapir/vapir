d5973217292482aaaf2ea295f8dd976af2e611dc
Ethan <ethan@medidata>
2011-04-26 14:05:20 -0400

    move FirefoxSocketError and its ancestors to the base FirefoxSocket file

--- vapir-firefox/lib/vapir-firefox/firefox_socket/jssh.rb
+++ vapir-firefox/lib/vapir-firefox/firefox_socket/jssh.rb
@@ -40,21 +40,6 @@ require 'vapir-firefox/javascript_object'
 
 # :startdoc:
 
-# base exception class for all exceptions raised from Jssh sockets and objects. 
-class FirefoxSocketError < StandardError;end
-# this exception covers all connection errors either on startup or during usage. often it represents an Errno error such as Errno::ECONNRESET. 
-class FirefoxSocketConnectionError < FirefoxSocketError;end
-# This exception is thrown if we are unable to connect to JSSh.
-class FirefoxSocketUnableToStart < FirefoxSocketConnectionError;end
-# Represents an error encountered on the javascript side, caught in a try/catch block. 
-class FirefoxSocketJavascriptError < FirefoxSocketError
-  attr_accessor :source, :js_err, :lineNumber, :stack, :fileName
-end
-# represents a syntax error in javascript. 
-class FirefoxSocketSyntaxError < FirefoxSocketJavascriptError;end
-# raised when a javascript value is expected to be defined but is undefined
-class FirefoxSocketUndefinedValueError < FirefoxSocketJavascriptError;end
-
 # A JsshSocket represents a connection to Firefox over a socket opened to the JSSH extension. It 
 # does the work of interacting with the socket and translating ruby values to javascript and back. 
 class JsshSocket < FirefoxSocket

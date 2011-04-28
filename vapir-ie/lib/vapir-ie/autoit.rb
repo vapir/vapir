5c0ed11feb412cb57ba7ea4d85ced50be5c49736
Ethan <ethan@medidata>
2011-04-28 15:41:22 -0400

    doc: autoit method

--- vapir-ie/lib/vapir-ie/autoit.rb
+++ vapir-ie/lib/vapir-ie/autoit.rb
@@ -1,5 +1,7 @@
 module Vapir
   AutoItDLL=File.join(File.expand_path(File.dirname(__FILE__)),'AutoItX3.dll')
+  # returns a WIN32OLE for an AutoIt control. if AutoIt is not registered, this will attempt to 
+  # registered the bundled AutoIt DLL and return a WIN32OLE for it when it is registered. 
   def self.autoit
     @@autoit||= begin
       begin

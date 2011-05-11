b1838db970efe2926127a31152e2997b46ad9601
Ethan <ethan@medidata>
2011-05-11 14:57:13 -0400

    drop jssh require; this isn't needed as it's done when the socket class is determined

--- vapir-firefox/lib/vapir-firefox/browser.rb
+++ vapir-firefox/lib/vapir-firefox/browser.rb
@@ -85,7 +85,6 @@ require 'vapir-common/waiter'
 require 'vapir-common/browser'
 require 'vapir-firefox/window'
 require 'vapir-firefox/modal_dialog'
-require 'vapir-firefox/firefox_socket/jssh'
 require 'vapir-firefox/container'
 require 'vapir-firefox/page_container'
 
